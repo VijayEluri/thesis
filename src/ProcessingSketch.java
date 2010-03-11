bd92bd06ad3777a8193d7904ab909811b792d78f
vady <vady@MacBook-Pro.local>
2010-03-11 20:34:21 +0100

    Testing on different data

--- src/ProcessingSketch.java
+++ src/ProcessingSketch.java
@@ -1,27 +1,26 @@
-import processing.core.PApplet;
 import edu.uci.ics.jung.graph.Graph;
 import io.GraphMLParser;
 import io.JungYedHandler;
-
-import java.io.File;
-import java.awt.geom.Point2D;
-
 import layout.RadialLayout;
+import processing.core.PApplet;
+
+import java.awt.geom.Point2D;
+import java.io.File;
 
 public class ProcessingSketch extends PApplet {
 
     private static final String TEST_GRAPH = "tree23.graphml";
-    private static final String REAL_GRAPH = "/Users/vady/Documents/sample/Processing/basic/RealClusterGraph.graphml";
-    private static final String CLUSTER_GRAPH = "/Users/vady/Documents/sample/Processing/basic/cluster.graphml";
+    private static final String REAL_GRAPH = "RealClusterGraph.graphml";
+    private static final String CLUSTER_GRAPH = "cluster.graphml";
 
     private RadialLayout layout;
 
     public static void main(String[] args) {
-        PApplet.main(new String[] { "--present", "ProcessingSketch" });
+        PApplet.main(new String[]{"--present", "ProcessingSketch"});
     }
 
     public void setup() {
-        
+
         size(800, 800);
         background(0);
         stroke(255);
@@ -34,7 +33,7 @@ public class ProcessingSketch extends PApplet {
     private Graph loadGraph() {
         GraphMLParser parser = new GraphMLParser(new JungYedHandler());
 
-        return (Graph) parser.load(new File(TEST_GRAPH)).get(0);
+        return (Graph) parser.load(new File(CLUSTER_GRAPH)).get(0);
     }
 
     public void draw() {
@@ -55,9 +54,9 @@ public class ProcessingSketch extends PApplet {
             Point2D end = layout.transform(graph.getDest(edge));
 
             line(new Float(start.getX()),
-                 new Float(start.getY()),
-                 new Float(end.getX()),
-                 new Float(end.getY()));
+                    new Float(start.getY()),
+                    new Float(end.getX()),
+                    new Float(end.getY()));
         }
     }
 
