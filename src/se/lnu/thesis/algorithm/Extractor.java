3a51a3192929195bb46e666654948f811767cb5c
vady <vady@MacBook-Pro.local>
2010-04-09 18:55:03 +0200

    Main subgraph extraction algorithm and tests

---
+++ src/se/lnu/thesis/algorithm/Extractor.java
@@ -0,0 +1,81 @@
+package se.lnu.thesis.algorithm;
+
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
+import edu.uci.ics.jung.graph.Graph;
+import se.lnu.thesis.core.MyGraph;
+import se.lnu.thesis.utils.GraphUtils;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vady
+ * Date: 07.04.2010
+ * Time: 23:04:50
+ * <p/>
+ * Main extraction rutine
+ * <p/>
+ * 1. Node - compute all leafs
+ * 2. Compute all nodes connected to each leaf from bottom to up
+ * 3. Merge. Find shared parent.
+ * 4. Create graph from this structure.
+ */
+public class Extractor {
+
+    public Graph extractSubGraph(MyGraph goGraph, MyGraph clusterGraph, Object goNode) {
+
+        Graph subGraph = new DirectedSparseGraph();
+
+        Object subGraphRoot = null;
+
+        for (Object goLeaf : GraphUtils.getInstance().getNodeLeafs(goGraph, goNode)) {
+
+            String label = goGraph.getLabel(goLeaf);
+
+            Set leafs = clusterGraph.getLeafsByLabel(label);
+
+            if (leafs.size() > 1) {
+                String error = "Error! Found couple leafs with label '" + label + "'!!";
+                System.out.println(error);
+                throw new IllegalStateException(error);
+            }
+
+            List connectedNodes = GraphUtils.getInstance().invertDfsNodes(clusterGraph, leafs.iterator().next());
+            for (int i = 0; i <= connectedNodes.size() - 1; i++) {
+                Object node1, node2 = null;
+
+                node1 = connectedNodes.get(i);
+                subGraph.addVertex(node1);
+
+                if (i + 1 <= connectedNodes.size() - 1) {
+                    node2 = connectedNodes.get(i + 1);
+
+                    subGraph.addVertex(node2);
+                    subGraph.addEdge(node2 + "->" + node1, node2, node1);
+                }
+
+            }
+
+            subGraphRoot = connectedNodes.get(connectedNodes.size() - 1); // TODO maybe use clusterGraph.getRoot() ?
+        }
+
+        optimize(subGraph, subGraphRoot);
+
+        return subGraph;
+    }
+
+    public void optimize(Graph graph, Object root) {
+        if (graph.outDegree(root) == 0) {
+            return;
+        }
+
+        if (graph.getSuccessorCount(root) == 1) {
+            Object next = graph.getSuccessors(root).iterator().next();
+
+            graph.removeVertex(root);
+            optimize(graph, next);
+        }
+    }
+
+}
---
+++ src/se/lnu/thesis/test/TestExtractor.java
@@ -0,0 +1,150 @@
+package se.lnu.thesis.test;
+
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
+import edu.uci.ics.jung.graph.Graph;
+import org.junit.Assert;
+import org.junit.Test;
+import se.lnu.thesis.algorithm.Extractor;
+import se.lnu.thesis.core.MyGraph;
+import se.lnu.thesis.io.GraphMLParser;
+import se.lnu.thesis.io.MyGraphYedHandler;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vady
+ * Date: 09.04.2010
+ * Time: 15:57:30
+ */
+public class TestExtractor {
+
+    /**
+     * ___1
+     * ___|
+     * ___2
+     * ___|
+     * ___3
+     * ___|
+     * ___4
+     * ___|
+     * ___5
+     * ___|
+     * ___6
+     * __/_\
+     * _7___8
+     * _____|
+     * _____9
+     * _____|
+     * ____10
+     *
+     * @return DirectedSparceGraph
+     */
+    private Graph createGraph() {
+        Graph graph = new DirectedSparseGraph();
+
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addVertex(4);
+        graph.addVertex(5);
+        graph.addVertex(6);
+        graph.addVertex(7);
+        graph.addVertex(8);
+        graph.addVertex(9);
+        graph.addVertex(10);
+
+        graph.addEdge("1->2", 1, 2);
+        graph.addEdge("2->3", 2, 3);
+        graph.addEdge("3->4", 3, 4);
+        graph.addEdge("4->5", 4, 5);
+        graph.addEdge("5->6", 5, 6);
+        graph.addEdge("6->7", 6, 7);
+        graph.addEdge("6->8", 6, 8);
+        graph.addEdge("8->9", 8, 9);
+        graph.addEdge("9->10", 9, 10);
+
+        return graph;
+    }
+
+    @Test
+    public void optimize() {
+
+        Graph graph = createGraph();
+
+        Assert.assertEquals(10, graph.getVertexCount());
+        Assert.assertEquals(9, graph.getEdgeCount());
+        Assert.assertTrue(graph.containsVertex(1));
+        Assert.assertTrue(graph.containsVertex(3));
+        Assert.assertTrue(graph.containsVertex(6));
+
+        Extractor extractor = new Extractor();
+        extractor.optimize(graph, 1);
+
+        Assert.assertEquals(5, graph.getVertexCount());
+        Assert.assertEquals(4, graph.getEdgeCount());
+        Assert.assertFalse(graph.containsVertex(1));
+        Assert.assertFalse(graph.containsVertex(3));
+        Assert.assertTrue(graph.containsVertex(6));
+    }
+
+    @Test
+    public void optimizeFromMiddle() {
+
+        Graph graph = createGraph();
+
+        Assert.assertEquals(10, graph.getVertexCount());
+        Assert.assertEquals(9, graph.getEdgeCount());
+
+        Assert.assertTrue(graph.containsVertex(1));
+        Assert.assertTrue(graph.containsVertex(3));
+        Assert.assertTrue(graph.containsVertex(6));
+
+        Extractor extractor = new Extractor();
+        extractor.optimize(graph, 4);
+
+        Assert.assertEquals(8, graph.getVertexCount());
+        Assert.assertEquals(6, graph.getEdgeCount());
+
+        Assert.assertTrue(graph.containsVertex(1));
+        Assert.assertTrue(graph.containsVertex(3));
+        Assert.assertTrue(graph.containsVertex(6));
+        Assert.assertFalse(graph.containsVertex(4));
+        Assert.assertFalse(graph.containsVertex(5));
+    }
+
+    @Test
+    public void extract() {
+
+        MyGraph goGraph = (MyGraph) new GraphMLParser(new MyGraphYedHandler()).load("RealGOGraph.graphml").get(0);
+        Assert.assertEquals(10042, goGraph.getVertexCount());
+        Assert.assertEquals(24155, goGraph.getEdgeCount());
+
+        MyGraph clusterGraph = (MyGraph) new GraphMLParser(new MyGraphYedHandler()).load("RealClusterGraph.graphml").get(0);
+        Assert.assertEquals(14623, clusterGraph.getVertexCount());
+        Assert.assertEquals(14622, clusterGraph.getEdgeCount());
+
+        Object node = goGraph.getNodeByLabel("tetrapyrrole metabolic process");
+        Assert.assertNotNull(node != null);
+        Assert.assertEquals("n7315", node);
+
+
+        Extractor extractor = new Extractor();
+
+        long start = System.currentTimeMillis();
+        Graph subGraph = extractor.extractSubGraph(goGraph, clusterGraph, "n7315");
+        long end = System.currentTimeMillis();
+
+        System.out.println("Extracting subgraph on real data. Done in " + (end - start) + "ms");
+
+        Assert.assertEquals(1699, subGraph.getVertexCount());
+/*
+        Assert.assertEquals(6, subGraph.getEdgeCount());
+
+
+        Assert.assertTrue(clusterGraph.containsVertex(3));
+        Assert.assertTrue(clusterGraph.containsVertex(6));
+        Assert.assertFalse(clusterGraph.containsVertex(4));
+        Assert.assertFalse(clusterGraph.containsVertex(5));
+*/
+    }
+
+}
