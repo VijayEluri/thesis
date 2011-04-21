a0f9eff930c366bc38ee0dce4279d176bda6e52b
Vlad <Vlad@MacBook.local>
2011-04-21 19:39:30 +0200

    Setup native Look&Feel for every platform.

--- src/main/java/GoClusterViz.java
+++ src/main/java/GoClusterViz.java
@@ -1,4 +1,7 @@
+import org.apache.log4j.Logger;
 import se.lnu.thesis.Scene;
+
+import javax.swing.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,11 +14,25 @@ import se.lnu.thesis.Scene;
  */
 public class GoClusterViz {
 
+    public static final Logger LOGGER = Logger.getLogger(GoClusterViz.class);
+
     /*
      * Program entry point.
      */
     public static void main(String[] args) {
         System.setProperty("apple.laf.useScreenMenuBar", "true");  // for MacOS X only
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            LOGGER.warn(e);
+        } catch (InstantiationException e) {
+            LOGGER.warn(e);
+        } catch (IllegalAccessException e) {
+            LOGGER.warn(e);
+        } catch (UnsupportedLookAndFeelException e) {
+            LOGGER.warn(e);
+        }
 
         Scene.getInstance().initUI();
     }
